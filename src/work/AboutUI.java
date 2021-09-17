027d7aae623eb682c04cbdafe6a7371a5cb2f23b
iamsourav37 <souravganguly108@gmail.com>
2021-09-17 09:18:15 +0530

    JOptionPane dialog box

--- src/work/AboutUI.java
+++ src/work/AboutUI.java
@@ -5,6 +5,8 @@
  */
 package work;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author iamso
@@ -28,7 +30,8 @@ public class AboutUI extends javax.swing.JFrame {
     private void initComponents() {
 
         aboutText = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        btnChangeText = new javax.swing.JButton();
+        btnShowMessage = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("About Author");
@@ -36,11 +39,18 @@ public class AboutUI extends javax.swing.JFrame {
         aboutText.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         aboutText.setText("Programmer of this app is Sourav Ganguly");
 
-        jButton1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jButton1.setText("Change Text");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnChangeText.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        btnChangeText.setText("Change Text");
+        btnChangeText.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnChangeTextActionPerformed(evt);
+            }
+        });
+
+        btnShowMessage.setText("Show Message");
+        btnShowMessage.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnShowMessageActionPerformed(evt);
             }
         });
 
@@ -54,30 +64,39 @@ public class AboutUI extends javax.swing.JFrame {
                         .addGap(65, 65, 65)
                         .addComponent(aboutText))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(144, 144, 144)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(78, Short.MAX_VALUE))
+                        .addGap(150, 150, 150)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(btnChangeText, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnShowMessage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap(159, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(138, 138, 138)
                 .addComponent(aboutText, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(39, 39, 39)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(56, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnChangeText, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(110, 110, 110)
+                .addComponent(btnShowMessage, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(109, Short.MAX_VALUE))
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnChangeTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnChangeTextActionPerformed
         // TODO add your handling code here:
         
         
         aboutText.setText("Sourav is the programmer behind this app !!!");
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnChangeTextActionPerformed
+
+    private void btnShowMessageActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnShowMessageActionPerformed
+        // TODO add your handling code here:
+        JOptionPane.showMessageDialog(AboutUI.this, "Hello user");
+    }//GEN-LAST:event_btnShowMessageActionPerformed
 
     /**
      * @param args the command line arguments
@@ -116,6 +135,7 @@ public class AboutUI extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel aboutText;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnChangeText;
+    private javax.swing.JButton btnShowMessage;
     // End of variables declaration//GEN-END:variables
 }
